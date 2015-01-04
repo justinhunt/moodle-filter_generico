75cb026bf0e3836ed6dbb17e17b82dd171cb9a4a
Justin Hunt <poodllsupport@gmail.com>
2015-01-05 00:07:44 +0900

    added more infor boxes

--- locallib.php
+++ locallib.php
@@ -134,7 +134,7 @@ class admin_setting_genericopresets extends admin_setting {
 	protected function fetch_presets(){
 
 	$ret = array();
-	$templates = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17);
+	$templates = array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19);
 	
 	foreach($templates as $templateno){
 		$presets = array();
