80155cce18634873887a8af7e7d37415296583ca
Justin Hunt <bitwalkerjapan@gmail.com>
2015-08-19 08:21:32 -0400

    a bug in the last release. Gah! how does it happen?

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015081901;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015081902;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011070100;        // Requires this Moodle version
 $plugin->component = 'filter_generico'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.2.1(Build 2015081901)';
+$plugin->release   = '1.2.2(Build 2015081902)';
