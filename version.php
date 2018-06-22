aba23c5dc5e5d91f40021a6fab38bb36f3360a69
justinhunt <bitwalkerjapan@gmail.com>
2018-06-22 17:08:48 +0900

    fixed settings label issue and bumped version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018041001;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2018062300;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011070100;        // Requires this Moodle version
 $plugin->component = 'filter_generico'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'Version 1.4.01(Build 2018041001)';
+$plugin->release   = 'Version 1.4.02(Build 2018062300)';
