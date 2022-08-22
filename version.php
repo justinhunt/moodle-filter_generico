e1aaa605d5681c52708c43b8df8863421e540b38
justinhunt <bitwalkerjapan@gmail.com>
2022-08-22 02:18:41 +0000

    added several new presets and a version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022062700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2022082200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2011070100;        // Requires this Moodle version
 $plugin->component = 'filter_generico'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Version 1.4.15(Build 2022062700)';
+$plugin->release = 'Version 1.4.16(Build 2022082200)';
